6a33d5cf6e77c5222bd0d242c820dff3dc10ab56
hakimlutfi46 <lutfihakim1606@gmail.com>
2023-09-12 05:17:37 +0700

    add acara 6 array

---
+++ Acara6_Array/array-basic.php
@@ -0,0 +1,15 @@
+<?php
+$pandawa = array("Arjuna", "Bima", "Nakula", "Sadewa", "Yudistira");
+
+echo $pandawa[0];
+echo "<br>";
+echo $pandawa[4];
+
+echo "<br>";
+$pandawa[1] = "Arjuna";
+$pandawa[2] = "Bima";
+$pandawa[3] = "Nakula";
+$pandawa[4] = "Sadewa";
+$pandawa[5] = "Yudistira";
+
+echo $pandawa[5];
